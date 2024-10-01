957ca1f5d5cc8282806171c5f9ffe5fba20e7b4e
Enrique Urra <enrique.urra@gmail.com>
2024-10-01 17:12:19 -0300

    Inicialización proyecto 2 - MVC: Conceptos básicos.

---
+++ Proyecto02/Controllers/CompraController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto02.Controllers
+{
+    public class CompraController : Controller
+    {
+        // GET: /Compra/AccionPrueba/
+        public string AccionPrueba()
+        {
+            return "Prueba controlador";
+        }
+
+        // GET: /Compra/
+        public IActionResult Index()
+        {
+            return View();
+        }        
+
+        // GET: /Compra/SeleccionEquipo/
+        public string SeleccionEquipo()
+        {
+            return "Paso I: selección equipo.";
+        }
+
+        // GET: /Compra/SeleccionModelo/
+        public string SeleccionModelo(string? nombre = "SIN NOMBRE")
+        {
+            return "Paso II: selección modelo para equipo : '" + nombre + "'";
+        }
+
+        // GET: /Compra/Confirmacion/
+        public string Confirmación()
+        {
+            return "Paso III: selección modelo";
+        }
+    }
+}
